6d4168834971282ab49dd81679fa163d7e081ed5
Dev Tuanoi <devtuanoi@gmail.com>
2021-07-23 15:14:29 +0700

    Radio Change

--- src/Components/Pages/RadioOnline.js
+++ src/Components/Pages/RadioOnline.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BuildWeb = () => {
     return (
         <div className="sec__one">
-            <h1>รับทำเว็บไซต์</h1>
+            <h1>จัดสถานีวิทยุออนไลน์</h1>
         </div>
     )
 }
